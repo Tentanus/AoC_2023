e6e31aafae622f58e74e66800cca78474325c1cb
tentanus <tentanusw@gmail.com>
2023-12-09 19:27:29 +0100

    day04: start

---
+++ day04/code.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <map>
+
+#define REPORT(X) std::cout << (X)
+
+int main()
+{
+    std::ifstream infile("input.txt");
+    std::string line;
+
+
+    while (std::getline(infile, line))
+    {
+        
+    }
+
+}
