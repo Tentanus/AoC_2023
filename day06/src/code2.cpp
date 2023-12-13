082a274a38a7377ea900e52e2502ac5a7953c197
tentanus <tentanusw@gmail.com>
2023-12-13 22:04:54 +0100

    day06: 2 finished

--- day06/src/code2.cpp
+++ day06/src/code2.cpp
@@ -1,27 +1,98 @@
 
-#include <code2.hpp>
+#include <algorithm>
+#include <cmath>
+#include <code.hpp>
+#include <utility>
+#include <vector>
 
 #define REPORT(X) std::cout << (X)
 
-int main(int argc, char* argv[])
-{
-    std::string file = "xinput.txt";
-    if (argc != 1)
-        file = argv[1];
-        
-    std::ifstream infile(file);
-    std::string line;
+int main(int argc, char *argv[]) {
+  std::string file = "xinput.txt";
+  if (argc != 1)
+    file = argv[1];
 
-    int res = 0;
-    while (std::getline(infile, line))
-    {
-        #ifdef DEBUG
-            std::cout   << "line:\t\t" << line << "\n";
-        #endif
-       
+  std::ifstream infile(file);
+  std::vector<long long> time;
+  std::vector<long long> dist;
 
+  {
+    std::string line_time;
+    std::string line_dist;
+
+    std::getline(infile, line_time);
+    std::getline(infile, line_dist);
+
+#ifdef DEBUG
+    std::cout << "time:\t|" << line_time << "\n";
+    std::cout << "dist:\t|" << line_dist << "\n";
+#endif
+
+    line_time = line_time.substr(9);
+    line_dist = line_dist.substr(9);
+
+    std::pair<size_t, size_t> p_time;
+    std::pair<size_t, size_t> p_dist;
+
+    p_time.first = line_time.find_first_of("0123456789");
+    p_dist.first = line_dist.find_first_of("0123456789");
+    p_time.second = line_time.find_first_of(" ", p_time.first);
+    p_dist.second = line_dist.find_first_of(" ", p_dist.first);
+
+    while (p_time.first != std::string::npos &&
+           p_dist.first != std::string::npos) {
+
+      time.push_back(std::stoll(
+          line_time.substr(p_time.first, p_time.second - p_time.first)));
+      dist.push_back(std::stoll(
+          line_dist.substr(p_dist.first, p_dist.second - p_dist.first)));
+
+      p_time.first = line_time.find_first_of("0123456789", p_time.second);
+      p_dist.first = line_dist.find_first_of("0123456789", p_dist.second);
+      p_time.second = line_time.find_first_of(" ", p_time.first);
+      p_dist.second = line_dist.find_first_of(" ", p_dist.first);
     }
+  }
 
-    REPORT("Result:\t") << res << std::endl;
-    return (1);
+  std::vector<int> arr;
+
+  for (size_t race = 0; race < time.size(); race++) {
+    int peaktime = time.at(race) / 2;
+    long long racewins = 1;
+    int hold;
+    
+    for (int i = 1;
+        dist.at(race) < -(peaktime + i) * ((peaktime + i) - time.at(race));
+        i += 1000)
+        {
+            racewins += 1000;
+            hold = i;
+        }
+    for (int i = hold;
+        dist.at(race) < -(peaktime + i) * ((peaktime + i) - time.at(race));
+        i++)
+            racewins++;
+
+
+    for (int i = 1;
+         dist.at(race) < -(peaktime - i) * ((peaktime - i) - time.at(race));
+        i += 1000)
+        {
+            racewins += 1000;
+            hold = i;
+        }
+    for (int i = hold;
+         dist.at(race) < -(peaktime - i) * ((peaktime - i) - time.at(race));
+        i++)
+            racewins++;
+
+    REPORT("RACE: ") << race << "\tWINS: " << racewins << std::endl;
+    arr.push_back(racewins);
+  }
+  int res = 1;
+  for (auto win : arr)
+    res *= win;
+
+  REPORT("RESULT: ") << res;
+  return (0);
 }
