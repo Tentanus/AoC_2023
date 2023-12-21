be41532711b8811f00775eb490dca56937d93593
Martijn Weverling <mweverli@student.codam.nl>
2023-12-21 16:08:28 +0100

    day07: 2 - finished

--- day07/inc/code-2.hpp
+++ day07/inc/code-2.hpp
@@ -1,122 +1,188 @@
 #ifndef CODE_HPP
-# define CODE_HPP
+#define CODE_HPP
 
 #include <fstream>
 #include <iostream>
 
 #include <unordered_map>
 
-enum class HV
-{
-	HIGH,				// 5
-	PAIR,				// 4
-	TWOPAIR,			// 3
-	THREEOFAKIND,		// 3
-	FULLHOUSE,			// 2
-	FOUROFAKIND,		// 2
-	FIVEOFAKIND			// 1
+enum class HV {
+  HIGH,         // 5
+  PAIR,         // 4
+  TWOPAIR,      // 3
+  THREEOFAKIND, // 3
+  FULLHOUSE,    // 2
+  FOUROFAKIND,  // 2
+  FIVEOFAKIND   // 1
 };
 
-HV getHandValue(const std::string hand)
-{
-	std::unordered_map<char, int> map;
-	for (size_t i = 0; i < hand.length(); i++)
-		map[hand.at(i)]++;
+HV getHandValue(const std::string hand) {
+  std::cout << " _val: ";
+  std::unordered_map<char, int> map;
+  for (size_t i = 0; i < hand.length(); i++)
+    map[hand.at(i)]++;
 
-	if (map.size() == 1)
-		return (HV::FIVEOFAKIND);
-	if (map.size() == 5)
-		return (HV::HIGH);
-	if (map.size() == 4)
-		return (HV::PAIR);
-	if (map.size() == 3 )
-	{
-		for (auto it : map)
-			if (it.second == 3)
-				return (HV::THREEOFAKIND);
-		return(HV::TWOPAIR);
-	}
-	if (map.size() == 2 )
-		for (auto it : map)
-			if (it.second == 3)
-				return (HV::FULLHOUSE);	
-	return (HV::FOUROFAKIND);
+  if (map.size() == 1)
+    return (HV::FIVEOFAKIND);
+  if (map.size() == 5)
+    return (HV::HIGH);
+  if (map.size() == 4)
+    return (HV::PAIR);
+  if (map.size() == 3) {
+    for (auto it : map)
+      if (it.second == 3)
+        return (HV::THREEOFAKIND);
+    return (HV::TWOPAIR);
+  }
+  if (map.size() == 2)
+    for (auto it : map)
+      if (it.second == 3)
+        return (HV::FULLHOUSE);
+  return (HV::FOUROFAKIND);
 }
 
-size_t getCardValue(const char letter)
-{
-	switch (letter)
-	{
-	case '2':
-		return (1);
-	case '3':
-		return (2);
-	case '4':
-		return (3);
-	case '5':
-		return (4);
-	case '6':
-		return (5);
-	case '7':
-		return (6);
-	case '8':
-		return (7);
-	case '9':
-		return (8);
-	case 'T':
-		return (9);
-	case 'J':
-		return (10);
-	case 'Q':
-		return (11);
-	case 'K':
-		return (12);
-	case 'A':
-		return (13);
-	}
-    std::cout << "REACHED END OF getCardValue";
-	return (SIZE_MAX);
+HV getHandJackValue(const std::string hand) {
+  std::cout << " Jval: ";
+  if (hand == "JJJJJ")
+    return (HV::FIVEOFAKIND);
+  std::unordered_map<char, int> map;
+
+  for (size_t i = 0; i < hand.length(); i++) {
+    map[hand.at(i)]++;
+  }
+  unsigned short jacks;
+  {
+    std::unordered_map<char, int>::iterator it = map.find('J');
+    jacks = it->second;
+    map.erase(it);
+  }
+
+  unsigned short nbr = 0;
+  for (const auto it : map) {
+    if (nbr < it.second)
+      nbr = it.second;
+  }
+
+  if (jacks == 1 && map.size() == 2 && nbr != 3)
+    return (HV::FULLHOUSE);
+
+  switch (nbr) {
+  case 1: {
+    switch (jacks) {
+    case 1:
+      return (HV::PAIR);
+    case 2:
+      return (HV::THREEOFAKIND);
+    case 3:
+      return (HV::FOUROFAKIND);
+    case 4:
+      return (HV::FIVEOFAKIND);
+    }
+  case 2: {
+    switch (jacks) {
+    case 1:
+      return (HV::THREEOFAKIND);
+    case 2:
+      return (HV::FOUROFAKIND);
+    case 3:
+      return (HV::FIVEOFAKIND);
+    }
+  }
+  case 3: {
+    switch (jacks) {
+    case 1:
+      return (HV::FOUROFAKIND);
+    case 2:
+      return (HV::FIVEOFAKIND);
+    }
+  }
+  case 4: {
+    switch (jacks) {
+    case 1:
+      return (HV::FIVEOFAKIND);
+    }
+  }
+  }
+  }
+  std::cout << "REACHED END OF getHandJackValue (hand: " << hand << ") ";
+  return (HV::HIGH);
 }
 
-std::string showVal(HV val)
-{
-	switch (val)
-	{
-	case HV::HIGH:
-		return ("NONE");
-	case HV::PAIR:
-		return ("PAIR");
-	case HV::TWOPAIR:
-		return ("TWO PAIR");
-	case HV::THREEOFAKIND:
-		return ("THREE OF A KIND");
-	case HV::FULLHOUSE:
-		return ("FULL HOUSE");
-	case HV::FOUROFAKIND:
-		return ("FOUR OF A KIND");
-	case HV::FIVEOFAKIND:
-		return ("FIVE OF A KIND");
-	default:
-		return ("FAULTY INPUT (showVal)");
-	}
+size_t getCardValue(const char letter) {
+
+  switch (letter) {
+  case 'J':
+    return (1);
+  case '2':
+    return (2);
+  case '3':
+    return (3);
+  case '4':
+    return (4);
+  case '5':
+    return (5);
+  case '6':
+    return (6);
+  case '7':
+    return (7);
+  case '8':
+    return (8);
+  case '9':
+    return (9);
+  case 'T':
+    return (10);
+  case 'Q':
+    return (11);
+  case 'K':
+    return (12);
+  case 'A':
+    return (13);
+  }
+  std::cout << "REACHED END OF getCardValue";
+  return (SIZE_MAX);
 }
 
-class CamelHand
-{
-	public:
-	std::string _cards;
-	std::string _bid;
-	HV _value;
+std::string valToString(HV val) {
+  switch (val) {
+  case HV::HIGH:
+    return ("NONE");
+  case HV::PAIR:
+    return ("PAIR");
+  case HV::TWOPAIR:
+    return ("TWO PAIR");
+  case HV::THREEOFAKIND:
+    return ("THREE OF A KIND");
+  case HV::FULLHOUSE:
+    return ("FULL HOUSE");
+  case HV::FOUROFAKIND:
+    return ("FOUR OF A KIND");
+  case HV::FIVEOFAKIND:
+    return ("FIVE OF A KIND");
+  default:
+    return ("FAULTY INPUT (valToString)");
+  }
+}
 
-	CamelHand(std::string line) {
-		size_t i = line.find_first_of(" ");
-		_cards = line.substr(0, i);
-		_bid = line.substr(i + 1);
+class CamelHand {
+public:
+  std::string _cards;
+  std::string _bid;
+  HV _value;
 
-		_value = getHandValue(_cards);
+  CamelHand(std::string line) {
+    size_t i = line.find_first_of(" ");
+    _cards = line.substr(0, i);
+    _bid = line.substr(i + 1);
 
-        std::cout << "CamelHand:\t" << _cards << " val: "  << showVal(_value) << std::endl;
-	}
+    std::cout << "CamelHand:\t" << _cards;
+
+    if (_cards.find("J") == std::string::npos)
+      _value = getHandValue(_cards);
+    else
+      _value = getHandJackValue(_cards);
+
+    std::cout << valToString(_value) << std::endl;
+  }
 };
 
 #endif
--- day07/src/code2.cpp
+++ day07/src/code2.cpp
@@ -1,96 +1,77 @@
 
-#include <code.hpp>
+#include <code-2.hpp>
 
 #include <array>
 #include <vector>
 
 #define REPORT(X) std::cout << (X)
 
-bool checkHandBiggerInst(const CamelHand &inst, const CamelHand &hand)
-{
-    for (size_t letter = 0 ; letter < inst._cards.length() ; letter++)
-    {
-        size_t inst_val = getCardValue(inst._cards.at(letter));
-        size_t inst_hand = getCardValue(hand._cards.at(letter));
-        if (inst_val == inst_hand)
-            continue;
-        if (inst_val < inst_hand)
-            return (true);
-        return (false);
-    }
-    REPORT("REACHED END OF checkInstBiggerHand");
+bool checkHandBiggerInst(const CamelHand &inst, const CamelHand &hand) {
+  for (size_t letter = 0; letter < inst._cards.length(); letter++) {
+    size_t inst_val = getCardValue(inst._cards.at(letter));
+    size_t inst_hand = getCardValue(hand._cards.at(letter));
+    if (inst_val == inst_hand)
+      continue;
+    if (inst_val < inst_hand)
+      return (true);
     return (false);
+  }
+  REPORT("REACHED END OF checkInstBiggerHand");
+  return (false);
 }
 
-void fittedemplace(std::vector<CamelHand> &list, CamelHand &hand)
-{
-    if (list.size() == 0)
-    {
-        list.emplace(list.begin(), hand);
-        return ;
-    }
-    for (std::vector<CamelHand>::iterator inst = list.begin(); inst < list.end() ; inst++)
-    {
-        if (checkHandBiggerInst(*inst, hand))
-            continue;
-        list.emplace(inst, hand);
-        return;
-    }
-    list.emplace_back(hand);
+void fittedemplace(std::vector<CamelHand> &list, CamelHand &hand) {
+  if (list.size() == 0) {
+    list.emplace(list.begin(), hand);
+    return;
+  }
+  for (std::vector<CamelHand>::iterator inst = list.begin(); inst < list.end();
+       inst++) {
+    if (checkHandBiggerInst(*inst, hand))
+      continue;
+    list.emplace(inst, hand);
+    return;
+  }
+  list.emplace_back(hand);
 }
 
-int main(int argc, char* argv[])
-{
-    std::string file = "xinput.txt";
-    if (argc != 1)
-        file = argv[1];
-        
-    std::array<std::vector<CamelHand>, 7> handlist;
+int main(int argc, char *argv[]) {
+  std::string file = "xinput.txt";
+  if (argc != 1)
+    file = argv[1];
 
-    std::ifstream infile(file);
-    std::string line;
+  std::array<std::vector<CamelHand>, 7> handlist;
 
-    size_t res = 0;
-    while (std::getline(infile, line))
-    {
-        #ifdef DEBUG
-//            std::cout   << "line:\t\t" << line << "\n";
-        #endif
+  std::ifstream infile(file);
+  std::string line;
 
-        CamelHand hand(line);
-        fittedemplace(handlist.at(static_cast<size_t>(hand._value)), hand);
+  size_t res = 0;
+  while (std::getline(infile, line)) {
+    if (line.empty() || line.find('#') != std::string::npos)
+      continue;
+    CamelHand hand(line);
+    fittedemplace(handlist.at(static_cast<size_t>(hand._value)), hand);
+  }
+
+  // PRINTING:
+  for (auto vecs : handlist) {
+    std::cout << "LIST:\t";
+    for (auto inst : vecs)
+      std::cout << inst._cards << " ";
+    std::cout << "\n" << std::endl;
+  }
+
+  // Calculate Result:
+  size_t mult = 1;
+  for (auto vecs : handlist) {
+    std::cout << "\nLIST:\t";
+    for (auto inst : vecs) {
+      std::cout << inst._bid << " * " << mult << " + ";
+      res += (std::stoll(inst._bid) * mult);
+      mult++;
     }
+  }
 
-    size_t mult = 1;
-    for (auto vecs : handlist)
-    {
-        std::cout << "LIST:\t";
-        for (auto inst : vecs)
-            std::cout << inst._cards << " ";
-        std::cout << "END" << std::endl;
-    }
-
-    mult = 1;
-    for (auto vecs : handlist)
-    {
-        std::cout << "\nLIST:\t";
-        for (auto inst : vecs)
-        {
-            std::cout << inst._bid << " * " << mult << " + ";
-            res += (std::stoll(inst._bid) * mult);
-            mult++;
-        }
-    }
-
-
-/*
-    go through file for each line:
-        create hand class and parse info:
-        add hand_val enum;
-
-    add it to an ordered list
-
-*/
-    REPORT("Result:\t") << res << std::endl;
-    return (0);
+  REPORT("\nResult:\t") << res << std::endl;
+  return (0);
 }
