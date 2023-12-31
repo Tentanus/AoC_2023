801ea825399b3abf21a304043f7df22713053a10
tentanus <tentanusw@gmail.com>
2023-12-31 16:53:09 +0100

    day09: 1 - start

---
+++ day09/src/code.cpp
@@ -0,0 +1,27 @@
+
+// clang-format off
+#include <code.hpp>
+
+#define REPORT(X) std::cout << (X)
+
+int main(int argc, char* argv[])
+{
+    std::string file = "xinput.txt";
+    if (argc != 1)
+        file = argv[1];
+        
+    std::ifstream infile(file);
+    std::string line;
+
+    int res = 0;
+    while (std::getline(infile, line))
+    {
+        #ifdef DEBUG
+            std::cout   << "line:\t\t" << line << "\n";
+        #endif
+    }
+
+    REPORT("Result:\t") << res << std::endl;
+    return (0);
+}
+// clang-format on
