3ac40de26f1a042491c89fb3b774949132e538a9
tentanus <tentanusw@gmail.com>
2024-01-02 00:40:14 +0100

    day09: 1&2 - finished

--- day09/src/code.cpp
+++ day09/src/code.cpp
@@ -4,18 +4,41 @@
 
 #define REPORT(X) std::cout << (X)
 
-long long delta(long long left, long long right)
+long verschil(long left, long right)
 {
-    return (left - right);
+    return (right - left);
 }
 
-void calculateHistory(const std::vector<long> &history, std::vector<long> &next)
+bool calculateHistory(std::vector<long> &history, std::vector<long> &next)
 {
+    bool flag = true;
+
     for (std::vector<long>::iterator it = history.begin() ;
-        it != history.end() - 1 ; it++)
+        it != (history.end() - 1) ; it++)
     {
-        next.emplace_back(delta(it*, (it + 1)*));
+        long delta = verschil(*it , *(it + 1));
+        next.emplace_back(delta);
+//        std::cout << std::to_string(delta) << " " ;
+        if (delta != 0)
+            flag = false;
     }
+//    std::cout << std::endl;
+    return (flag);
+}
+
+long predictnext(std::vector<long> history)
+{
+    std::vector<long> next;
+
+    if (calculateHistory(history, next))
+    {
+//        std::cout << "LOWEST LINE" << std::endl;
+        return (history.at(history.size() - 1));
+    }
+    long val = predictnext(next);
+
+//    std::cout << "VAL: " << std::to_string(val) << std::endl;
+    return (history.at(history.size() - 1) + val);
 }
 
 int main(int argc, char* argv[])
@@ -27,8 +50,7 @@ int main(int argc, char* argv[])
     std::ifstream infile(file);
     std::string line;
 
-    int res = 0;
-    std::vector<long> history;
+    long res = 0;
 
     while (std::getline(infile, line))
     {
@@ -37,10 +59,14 @@ int main(int argc, char* argv[])
         #endif
         std::stringstream ss(line);
         std::string option;
+
+        std::vector<long> history;
+        std::vector<long> next;
+
         for ( ; std::getline(ss, option, ' ') ; )
-        {
                 history.emplace_back(std::stoll(option));
-        }
+        
+        res += predictnext(history);
     }
 
     REPORT("Result:\t") << res << std::endl;
--- day09/src/code2.cpp
+++ day09/src/code2.cpp
@@ -1,7 +1,45 @@
+
 // clang-format off
-#include <code-2.hpp>
+#include <code.hpp>
 
 #define REPORT(X) std::cout << (X)
+
+long verschil(long left, long right)
+{
+    return (right - left);
+}
+
+bool calculateHistory(std::vector<long> &history, std::vector<long> &next)
+{
+    bool flag = true;
+
+    for (std::vector<long>::iterator it = history.begin() ;
+        it != (history.end() - 1) ; it++)
+    {
+        long delta = verschil(*it , *(it + 1));
+        next.emplace_back(delta);
+//        std::cout << std::to_string(delta) << " " ;
+        if (delta != 0)
+            flag = false;
+    }
+//    std::cout << std::endl;
+    return (flag);
+}
+
+long predictnext(std::vector<long> history)
+{
+    std::vector<long> next;
+
+    if (calculateHistory(history, next))
+    {
+//        std::cout << "LOWEST LINE" << std::endl;
+        return (history.at(0));
+    }
+    long val = predictnext(next);
+
+//    std::cout << "VAL: " << std::to_string(val) << std::endl;
+    return (history.at(0) - val);
+}
 
 int main(int argc, char* argv[])
 {
@@ -12,12 +50,23 @@ int main(int argc, char* argv[])
     std::ifstream infile(file);
     std::string line;
 
-    int res = 0;
+    long res = 0;
+
     while (std::getline(infile, line))
     {
         #ifdef DEBUG
             std::cout   << "line:\t\t" << line << "\n";
         #endif
+        std::stringstream ss(line);
+        std::string option;
+
+        std::vector<long> history;
+        std::vector<long> next;
+
+        for ( ; std::getline(ss, option, ' ') ; )
+                history.emplace_back(std::stoll(option));
+        
+        res += predictnext(history);
     }
 
     REPORT("Result:\t") << res << std::endl;
